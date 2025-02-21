dc923b4dd9797f07b51f805a5ae1ca0882b203f8
Jatin Gandhi <goldcoinjatin@gmail.com>
2025-02-21 11:56:55 +0530

    content changes

--- src/components/Data.ts
+++ src/components/Data.ts
@@ -1,4 +1,4 @@
-export const FirstHalfContentString = "My name is KaizokuJG aka Jatin. KaizokuJG is my alias which I usually use online. I like to code, play video games, watch anime, manga, blah blah blah.... I'm just an introvert and a nerd. I took this design inspiration from Kamaboko."
+export const FirstHalfContentString = "My name is KaizokuJG. KaizokuJG is my alias which I usually use online. I like to code, play video games, watch anime, manga. I'm just an introvert and a nerd. I took this design inspiration from Kamaboko."
 
 export const FirstHalfContentHeading = "Hello, Nice to meet you"
 
@@ -6,7 +6,7 @@ export const FirstHalfHeadingName = "ABOUT ME"
 
 export const secondHalfHeadingName = "My Experience"
 export const secondHalfContentHeading = "UI Developer"
-export const secondHalfContentText = "I'm a UI Developer currently working in Cognizant. I have 1 year of experience. I use ReactJS along with some libraries such as GSAP for animation and ThreeJS and React three fiber for adding 3d models."
+export const secondHalfContentText = "I'm a UI Developer. I have 1 year of experience. I use ReactJS along with some libraries such as GSAP for animation and ThreeJS and React three fiber for adding 3d models."
 
 
 export const thirdHalfHeadingName = "LETS CONNECT"
